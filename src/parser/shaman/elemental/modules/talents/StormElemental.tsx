28d01a29721070cb15dc504889a778459dd8f3df
David Grölle <david.groelle@protonmail.com>
2020-02-14 19:28:24 +0100

    Update SE logic

--- src/parser/shaman/elemental/modules/talents/StormElemental.tsx
+++ src/parser/shaman/elemental/modules/talents/StormElemental.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 import SPELLS from 'common/SPELLS';
 import SpellIcon from 'common/SpellIcon';
-import { formatNumber, formatPercentage } from 'common/format';
+import { formatPercentage } from 'common/format';
 
-import Analyzer from 'parser/core/Analyzer';
+import Analyzer, { SELECTED_PLAYER } from 'parser/core/Analyzer';
 import EnemyInstances from 'parser/shared/modules/EnemyInstances';
 
-import StatisticBox, { STATISTIC_ORDER } from 'interface/others/StatisticBox';
+import StatisticBox from 'interface/others/StatisticBox';
 
 import Abilities from '../Abilities';
-import { CastEvent } from '../../../../core/Events';
+import Events, { CastEvent, RemoveBuffEvent } from '../../../../core/Events';
 import Combatant from '../../../../core/Combatant';
 
 type bla = (combatant: Combatant)=> number;
@@ -22,55 +22,62 @@ class StormElemental extends Analyzer {
     enemies: EnemyInstances,
   };
 
-
+  protected acceptableSpells:Array<Function> = [];
 
   protected readonly abilities!: Abilities;
-  protected readonly  enemies!: EnemyInstances;
+  protected readonly enemies!: EnemyInstances;
 
   _resolveAbilityGcdField(value: any): number {
-    if ((value as bla).call(this.owner, this.selectedCombatant)){
-      return (value as bla).call(this.owner, this.selectedCombatant);
+    if (typeof value === 'function') {
+      return value.call(this.owner, this.selectedCombatant);
+    } else {
+      return value
     }
-    if (typeof value === "number"){
-      return value;
-    }
-    throw new Error("Expected number or function");
   }
 
-  onlyLightningBolt(event: CastEvent) {
-    return event.ability.guid !== SPELLS.LIGHTNING_BOLT.id;
+  get WindGustStacks() {
+    const buff:any = this.selectedCombatant.getBuff(SPELLS.WIND_GUST_BUFF.id);
+      if(!buff){
+        return 0;
+      } else {
+        return buff.stacks;
+    }
   }
 
   onlySurges(event: CastEvent) {
-    if (this.selectedCombatant.hasBuff(SPELLS.LAVA_SURGE_BUFF.id)) {
-      return event.ability.guid === SPELLS.LAVA_BURST.id;
-    } else {
-      return event.ability.guid === SPELLS.LIGHTNING_BOLT.id;
-    }
+    this.acceptableSpells.push(
+      (event: CastEvent, combatant: Combatant)=>(
+        event.ability.guid===SPELLS.LAVA_BURST.id &&
+        combatant.hasBuff(SPELLS.LAVA_SURGE.id)
+      ),
+    );
+    this.numCasts[SPELLS.LAVA_BURST.id]=0;
   }
 
-  lavaBurstOrLightningBolt(event: CastEvent) {
-    if (event.ability.guid === SPELLS.LAVA_BURST.id) {
-      return true;
-    }
-    if (event.ability.guid === SPELLS.LIGHTNING_BOLT.id) {
-      return true;
-    }
-    return false;
+  lavaBurst() {
+    this.acceptableSpells.push(
+      (event: CastEvent, combatant: Combatant)=>(event.ability.guid===SPELLS.LAVA_BURST.id),
+    );
+    this.numCasts[SPELLS.LAVA_BURST.id]=0;
+  }
+
+  elementalBlast() {
+    this.acceptableSpells.push(
+      (event: CastEvent, combatant: Combatant)=>(
+        event.ability.guid===SPELLS.ELEMENTAL_BLAST_TALENT.id &&
+      this.WindGustStacks<14),
+    );
+    this.numCasts[SPELLS.ELEMENTAL_BLAST_TALENT.id] = 0;
   }
 
   castChecksByIP: {[id: number]: Function} = {
-    0: this.onlyLightningBolt,
-    1: this.onlyLightningBolt,
     2: this.onlySurges,
-    3: this.lavaBurstOrLightningBolt,
+    3: this.lavaBurst,
   };
 
-  correctCastCheck : Function;
-
-  badFS = 0;
   justEnteredSE = false;
-  checkDelay = 0;
+  wrongCasts = 0;
+  correctCasts = 0;
 
   numCasts = {
     [SPELLS.STORM_ELEMENTAL_TALENT.id]: 0,
@@ -85,12 +92,34 @@ class StormElemental extends Analyzer {
     super(options);
     this.active
       = this.selectedCombatant.hasTalent(SPELLS.STORM_ELEMENTAL_TALENT.id);
-    const ipTraits = this.selectedCombatant.traitsBySpellId[SPELLS.IGNEOUS_POTENTIAL.id];
-    if(ipTraits === undefined){
-      this.correctCastCheck = this.castChecksByIP[0];
-    } else {
-      this.correctCastCheck = this.castChecksByIP[ipTraits.length];
+    this.acceptableSpells.push(
+      (event: CastEvent, combatant: Combatant)=>(event.ability.guid===SPELLS.LIGHTNING_BOLT.id),
+    );
+    this.acceptableSpells.push(
+      (event: CastEvent, combatant: Combatant)=>(event.ability.guid===SPELLS.CHAIN_LIGHTNING.id),
+    );
+    this.acceptableSpells.push(
+      (event: CastEvent, combatant: Combatant)=>(event.ability.guid===SPELLS.EARTH_SHOCK.id),
+    );
+    this.acceptableSpells.push(
+      (event: CastEvent, combatant: Combatant)=>(event.ability.guid===SPELLS.EARTHQUAKE.id),
+    );
+    const nhTraits = this.selectedCombatant.traitsBySpellId[SPELLS.IGNEOUS_POTENTIAL.id];
+    if(nhTraits && nhTraits.length===3){
+      this.elementalBlast();
     }
+    this.addEventListener(
+      Events.cast.by(SELECTED_PLAYER),
+      this.onCastDuringSE,
+    );
+    this.addEventListener(
+      Events.removebuff.by(SELECTED_PLAYER).spell(SPELLS.WIND_GUST_BUFF),
+      this.onWindGustRemove,
+    );
+    this.addEventListener(
+      Events.cast.by(SELECTED_PLAYER).spell(SPELLS.STORM_ELEMENTAL_TALENT),
+      this.onSECast,
+    );
   }
 
   get stormEleUptime() {
@@ -98,33 +127,21 @@ class StormElemental extends Analyzer {
       this.owner.fightDuration;
   }
 
-  get averageLightningBoltCasts() {
-    return (
-      this.numCasts[SPELLS.LIGHTNING_BOLT.id] /
-      this.numCasts[SPELLS.STORM_ELEMENTAL_TALENT.id]
-    ) || 0;
-  }
-
-  get averageChainLightningCasts() {
-    return (
-      this.numCasts[SPELLS.CHAIN_LIGHTNING.id] /
-      this.numCasts[SPELLS.STORM_ELEMENTAL_TALENT.id]
-    ) || 0;
+  onWindGustRemove(event: RemoveBuffEvent){
+    this.justEnteredSE = false;
   }
 
   onSECast(event: CastEvent) {
     this.justEnteredSE = true;
     this.numCasts[SPELLS.STORM_ELEMENTAL_TALENT.id] += 1;
+  }
 
-    const ability = this.abilities.getAbility(event.ability.guid);
-    if (!ability) {
+  onCastDuringSE(event: CastEvent) {
+    if(!this.justEnteredSE){
       return;
     }
-
-    if (!this.selectedCombatant.hasBuff(
-      SPELLS.WIND_GUST_BUFF.id,
-      event.timestamp,
-    )) {
+    const ability = this.abilities.getAbility(event.ability.guid);
+    if (!ability) {
       return;
     }
 
@@ -132,25 +149,31 @@ class StormElemental extends Analyzer {
     if (!gcd) {
       return;
     }
+
+    if(this.acceptableSpells.some(x=>x(event, this.selectedCombatant))) {
+      this.correctCasts++;
+    } else {
+      this.wrongCasts++;
+      console.log(event.ability.guid);
+    }
+
   }
 
   statistic() {
     return (
       <StatisticBox
         icon={<SpellIcon id={SPELLS.STORM_ELEMENTAL_TALENT.id} />}
-        value={`${formatNumber(this.averageLightningBoltCasts)}`}
-        label="Average Number Of Lightning Bolts per Storm Elemental Cast"
+        value={`${formatPercentage(this.correctCasts/(this.correctCasts+this.wrongCasts))}`}
+        label="Percentag of Correct Casts during hard cast Storm Elemental"
         tooltip={(
           <>
             With a uptime of: {formatPercentage(this.stormEleUptime)} %<br />
             Casts while Storm Elemental was up:
             <ul>
-              <li>Earth Shock: {this.numCasts[SPELLS.EARTH_SHOCK.id]}</li>
-              <li>Lightning Bolt: {this.numCasts[SPELLS.LIGHTNING_BOLT.id]}</li>
-              <li>Earthquake: {this.numCasts[SPELLS.EARTHQUAKE.id]}</li>
-              <li>Chain Lightning: {this.numCasts[SPELLS.CHAIN_LIGHTNING.id]}</li>
-              <li>Other Spells: {this.numCasts.others}</li>
+              <li>Correct Casts: {this.correctCasts}</li>
+              <li>Wrong Casts: {this.wrongCasts}</li>
             </ul>
+            Refer to https://stormearthandlava.com for the correct casts.
           </>
         )}
       />
@@ -160,24 +183,23 @@ class StormElemental extends Analyzer {
   get suggestionTresholds() {
     return {
       actual: this.numCasts.others,
-      isGreaterThan: {
-        minor: 0,
-        major: 1,
+      isLessThan: {
+        minor: 1,
+        major: 0.95,
       },
-      style: 'absolute',
+      style: 'percentage',
     };
   }
 
   suggestions(when: any) {
-    const abilities = `Lightning Bolt/Chain Lightning and Earth Shock/Earthquake`;
     when(this.suggestionTresholds)
       .addSuggestion(
         (suggest: any, actual: any, _: any) => {
           return suggest(
-            <span>Maximize your damage during Storm Elemental by only using {abilities}.</span>)
+            <span>Maximize your damage during Storm Elemental by only using the correct abilities.</span>)
             .icon(SPELLS.STORM_ELEMENTAL_TALENT.icon)
             .actual(`${actual} other casts with Storm Elemental up`)
-            .recommended(`Only cast ${abilities} while Storm Elemental is up.`);
+            .recommended(`Only cast specific while Storm Elemental is up.`);
         });
   }
 }
